91c0b945a8494ed95073f3c14d72a730b0d430e4
boriskzd <boriskzd@users.noreply.github.com>
2017-05-18 11:15:19 +0200

    Delete connect.php

--- verzija 1.0/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//konektiranje s bazom podataka
-$conn = mysqli_connect('localhost','root','1234','prijava');
-//provjera konekcije
-if (!$conn){
-  die("Connection failed: " . mysqli_connect_error()); //connection eror - injection hacking! maknuti prije live websajta
-};
