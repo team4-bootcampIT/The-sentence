0b4d3604c2ca27854558efa7b55da52cda6c1772
marko443 <marko.tomaic123@gmail.com>
2017-05-19 11:53:34 +0200

    Cookies"koristimo" dizajn i poruka
    Warning za cookies kada dodes prvi put na stranicu da ti kaže da kroistimo cookie

--- header.php
+++ header.php
@@ -1,11 +1,30 @@
 <?php
-    session_start();
-?>
+
+session_start();
+
+
+ ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <base href="http://localhost/">
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#014d83"
+    },
+    "button": {
+      "background": "#2196f3"
+    }
+  },
+  "theme": "classic"
+})});
+</script>
+    <base href="http://the-sentence.ga/">
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,6 +32,6 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans|Open+Sans:300,400" rel="stylesheet">
     <script src="https://use.fontawesome.com/f24e56ad22.js"></script>
-</head>
 
+</head>
 <body>
