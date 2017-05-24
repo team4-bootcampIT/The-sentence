4887f56f541c6f575f5ee8cb1a165bdd0276db8e
IKoporcic <ina_koporcic@hotmail.com>
2017-05-24 13:42:24 +0200

    Komentar za SQL injection protection

--- checkEmail.php
+++ checkEmail.php
@@ -3,11 +3,12 @@ include "connect.php";
 
  // $email  = $_REQUEST['email'];
 
+// SQL injection protection - ovo je jedan način ali je slabija zaštita -> mysqli_real_escape_string 
 $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
 
 
 
-
+// SQL injection protection - ovo je drugi način i bolja zaštita -> prepared statements
 $stmt=$conn->prepare( "SELECT * FROM prijava WHERE  email=? ");
 $stmt->bind_param("s",$email);
 
