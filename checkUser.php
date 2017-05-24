3bf9ec5117db0b9ae298d9b3547f687fe5fe2ef6
IKoporcic <ina_koporcic@hotmail.com>
2017-05-24 13:43:54 +0200

    Komentar za SQL injection protection

--- checkUser.php
+++ checkUser.php
@@ -3,11 +3,12 @@ include "connect.php";
 
  // $username  = $_REQUEST['username'];
 
+ // SQL injection protection - ovo je jedan način ali je slabija zaštita -> mysqli_real_escape_string
  $username = mysqli_real_escape_string($conn, $_REQUEST['username']);
 
 
 
-
+// SQL injection protection - ovo je drugi način i bolja zaštita -> prepared statements
 $stmt=$conn->prepare( "SELECT * FROM prijava WHERE  username=? ");
 $stmt->bind_param("s",$username);
 
