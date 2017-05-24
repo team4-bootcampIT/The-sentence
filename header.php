fc17d452ec18a5396653413e7167851542866564
IKoporcic <ina_koporcic@hotmail.com>
2017-05-25 00:58:46 +0200

    Upisani Google platform library i client ID

--- header.php
+++ header.php
@@ -13,10 +13,11 @@ include "include/direct.php";
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="google-signin-client_id" content="252015222418-qq255jpdirkdc19m8utsn9imcj7vjj0v.apps.googleusercontent.com"> <!-- client ID -->
     <title>The Sentence Login</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans|Open+Sans:300,400" rel="stylesheet"> 
     <script src="https://use.fontawesome.com/f24e56ad22.js"></script>
- 
+	<script src="https://apis.google.com/js/platform.js" async defer></script> <!-- Google platform library -->
 </head>
 <body>
