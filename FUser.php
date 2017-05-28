b8a6671c1f97c0bc1bd37ada1476539c1af4f265
marko443 <marko.tomaic123@gmail.com>
2017-05-28 18:28:54 +0200

    dodao automatsko kreiranje accounta pritiskom gumba login with facebook. kreira account za sad username=ime i password=123

--- FUser.php
+++ FUser.php
@@ -26,11 +26,11 @@ class User {
 			$prevResult = $this->db->query($prevQuery);
 			if($prevResult->num_rows > 0){
 				// Update podataka  u bazi "vazno za datume  i tokene"
-				$query = "UPDATE ".$this->userTbl." SET first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', modified = '".date("Y-m-d H:i:s")."' WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'";
+				$query = "UPDATE ".$this->userTbl." SET first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."',username = '".$userData['first_name']."',password='$2y$10$3ShY3d693ca0qOZHyXjoBOC0a1LM.pjWgrpV7GxxuU.jTjUzRhpdK',active='1', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', modified = '".date("Y-m-d H:i:s")."' WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'";
 				$update = $this->db->query($query);
 			}else{
 				// Dodavanje novih ako neki fale
-				$query = "INSERT INTO ".$this->userTbl." SET oauth_provider = '".$userData['oauth_provider']."', oauth_uid = '".$userData['oauth_uid']."', first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', created = '".date("Y-m-d H:i:s")."', modified = '".date("Y-m-d H:i:s")."'";
+				$query = "INSERT INTO ".$this->userTbl." SET oauth_provider = '".$userData['oauth_provider']."', oauth_uid = '".$userData['oauth_uid']."', first_name = '".$userData['first_name']."', username = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', created = '".date("Y-m-d H:i:s")."', modified = '".date("Y-m-d H:i:s")."'";
 				$insert = $this->db->query($query);
 			}
 			// Povlacenje podataka iz baze
