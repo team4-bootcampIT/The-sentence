f2f99c053b3cb037a9600e43a4ea45d857830e0b
marko443 <marko.tomaic123@gmail.com>
2017-05-28 19:20:10 +0200

    bugfix baza

--- db.sql
+++ db.sql
@@ -1,21 +1,9 @@
-CREATE TABLE `prijava` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `oauth_provider` enum('','facebook','google') COLLATE utf8_unicode_ci NOT NULL,
-  `oauth_uid` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `password` varchar(245) DEFAULT NULL,
-  `active` int(11) DEFAULT NULL,
-  `email_code` varchar(245) DEFAULT NULL,
-  `member_id` varchar(245) DEFAULT NULL,
-  `member_token` varchar(245) DEFAULT NULL,
-  `gender` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `locale` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-)
+/*
+-- Query: SELECT * FROM testbaza_facebook.prijava
+LIMIT 0, 1000
+
+-- Date: 2017-05-28 13:00
+*/
+INSERT INTO `prijava` (`id`,`oauth_provider`,`oauth_uid`,`email`,`first_name`,`last_name`,`username`,`password`,`active`,`email_code`,`member_id`,`member_token`,`gender`,`locale`,`picture`,`link`,`created`,`modified`) VALUES (1,'','','overlord443@gmail.com','','','marko443','$2y$10$JNnF8gAeD9OIZkSrL1JxLOaWzaaxTR7ZIhwH2PvKnRSrEBHhwkjLK',1,'a05b93c5c9881ec244c8462fe38aa0c4',NULL,NULL,'','','','','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `prijava` (`id`,`oauth_provider`,`oauth_uid`,`email`,`first_name`,`last_name`,`username`,`password`,`active`,`email_code`,`member_id`,`member_token`,`gender`,`locale`,`picture`,`link`,`created`,`modified`) VALUES (2,'','','mrvica@gmail.com','','','mrvica','$2y$10$3ShY3d693ca0qOZHyXjoBOC0a1LM.pjWgrpV7GxxuU.jTjUzRhpdK',1,'5f51fdaadd86e9d28f75be6cf5a3c63c',NULL,NULL,'','','','','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `prijava` (`id`,`oauth_provider`,`oauth_uid`,`email`,`first_name`,`last_name`,`username`,`password`,`active`,`email_code`,`member_id`,`member_token`,`gender`,`locale`,`picture`,`link`,`created`,`modified`) VALUES (3,'facebook','10213046425539950','','Jules','Winnfield','Jules','$2y$10$3ShY3d693ca0qOZHyXjoBOC0a1LM.pjWgrpV7GxxuU.jTjUzRhpdK',1,NULL,NULL,NULL,'male','en_US','https://scontent.xx.fbcdn.net/v/t1.0-1/c0.0.50.50/p50x50/18671279_10213049511617100_2135501846684248883_n.jpg?oh=e2dff5444a713785f3d5a9cefacd3d6d&oe=59AFB7C1','https://www.facebook.com/app_scoped_user_id/10213046425539950/','2017-05-28 10:46:42','2017-05-28 12:17:33');
