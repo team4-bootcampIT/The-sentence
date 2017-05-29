1025dee68c5bd7370b424752faa4f1464c5478d5
boriskzd <boriskzd@users.noreply.github.com>
2017-05-29 11:26:12 +0200

    dodan je <main> element zbog poravnanja, sad je bolja responzivnost i ima rubove

--- verification.php
+++ verification.php
@@ -6,17 +6,16 @@ include "nav.php";
 
 
 <body>
-  <div id="login-container">
-
-      <form id="form-container" action="include/login.include.php" method="POST">
-
-<p>Verification email has been sent!<br>Check you email and spam folder</p>
+    <main>
+        <div id="login-container">
+            <form id="form-container" action="include/login.include.php" method="POST">
+                <p>Verification email has been sent!<br>Check you email and spam folder</p>
+                   </form>
+                   <form action="login.php">
+               <button  id="submit-button">Go to log in</button>
            </form>
-           <form action="login.php">
-       <button  id="submit-button">Go to log in</button>
-   </form>
-          </div>
-
+       </div>
+   </main>
 
 
 <?php
